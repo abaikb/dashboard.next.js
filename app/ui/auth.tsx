f420dcedcf63e036fcc5454c24aa5835a985d9df
abaikb <abaikb@icloud.com>
2024-04-03 16:17:07 +0600

    add auth form

--- app/ui/auth.tsx
+++ app/ui/auth.tsx
@@ -1,5 +1,5 @@
 'use client';
- 
+
 import { lusitana } from '@/app/ui/fonts';
 import {
   AtSymbolIcon,
@@ -9,16 +9,16 @@ import {
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from '@/app/ui/button';
 import { useFormState, useFormStatus } from 'react-dom';
-import { authenticate } from '@/app/lib/actions';
- 
-export default function LoginForm() {
-  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
- 
+import { registerUser } from '@/app/lib/actions';
+
+export default function RegisterForm() {
+  const [errorMessage, dispatch] = useFormState(registerUser, undefined);
+
   return (
-    <form action={dispatch} className="space-y-3">
+    <form onSubmit={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-          Please log in to continue.
+          Please register to continue.
         </h1>
         <div className="w-full">
           <div>
@@ -61,7 +61,7 @@ export default function LoginForm() {
             </div>
           </div>
         </div>
-        <LoginButton />
+        <RegisterButton />
         <div
           className="flex h-8 items-end space-x-1"
           aria-live="polite"
@@ -70,7 +70,6 @@ export default function LoginForm() {
           {errorMessage && (
             <>
               <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-              <p className="text-sm text-red-500">{errorMessage}</p>
             </>
           )}
         </div>
@@ -78,13 +77,13 @@ export default function LoginForm() {
     </form>
   );
 }
- 
-function LoginButton() {
+
+function RegisterButton() {
   const { pending } = useFormStatus();
- 
+
   return (
-    <Button className="mt-4 w-full" aria-disabled={pending}>
-      Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+    <Button type="submit" className="mt-4 w-full" aria-disabled={pending}>
+      Register <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
     </Button>
   );
 }
