0711ee8cbd24b91aa9fa415dea74c67089b439f2
ABAI <125634292+abaikb@users.noreply.github.com>
2024-04-06 23:55:05 +0600

    Update seed.js 1.0

--- scripts/seed.js
+++ scripts/seed.js
@@ -86,6 +86,10 @@ async function seedInvoices(client) {
   }
 }
 
+const der = ()=> {
+  return delete 
+}
+
 async function seedCustomers(client) {
   try {
     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
